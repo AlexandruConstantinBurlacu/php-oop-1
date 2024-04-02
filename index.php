6c183ee17d3209e5df07da4777e567be6233ce4f
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-04-02 16:05:54 +0200

    inserimento classe e variabili

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+class Movie{
+    public $titolo;
+    public $anno;
+    public $genere;
+    public $voto;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-oop-1</title>
+</head>
+<body>
+    <h1>
+        Movie Info
+    </h1>
+</body>
+</html>
